9cc3dbf04a72ee9614ef0f511f87236630e4517d
clementg13 <grosieux.clement@gmail.com>
2024-01-18 16:58:28 +0100

    Commit of the project structure

---
+++ src/main/java/com/dev/apitestdevlogiciel/ApiTestDevLogicielApplication.java
@@ -0,0 +1,13 @@
+package com.dev.apitestdevlogiciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestDevLogicielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestDevLogicielApplication.class, args);
+    }
+
+}
