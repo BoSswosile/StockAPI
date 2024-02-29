b888720b4cdf4b3063dc09d0af1e3eb851950e61
Jerem-LBN <lebihanjeremie2@gmail.com>
2024-02-29 15:39:33 +0100

    Add getById and search function

--- src/main/java/com/stockapi/StockAPI/controller/StockController.java
+++ src/main/java/com/stockapi/StockAPI/controller/StockController.java
@@ -4,12 +4,14 @@ import com.stockapi.StockAPI.model.Product;
 import com.stockapi.StockAPI.model.Table;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.desktop.SystemEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +28,7 @@ public class StockController {
     @GetMapping
     public ResponseEntity<List<Product>> getProduct() {
         List<Product> product = new ArrayList<>();
-        Table table = Table.builder().id(1).name("Table de kevin").price(100).quantity(10).length(100).width(100).height(100).color("black").build();
+        Table table = Table.builder().id(1).name("Table de Fred").price(100).quantity(10).length(100).width(100).height(100).color("black").build();
         products.add(table);
         for (Product prod : products) {
             product.add(modelMapper.map(prod, Product.class));
@@ -58,6 +60,28 @@ public class StockController {
         for (Product prod : products) {
             if (prod.getId() == id) {
                 products.remove(prod);
+                return new ResponseEntity<>(prod, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping ("/search/{name}")
+    public ResponseEntity<List<Product>> searchProduct(@PathVariable String name){
+        List<Product> product = new ArrayList<>();
+        for(Product prod : products){
+            if(prod.getName().contains(name)){
+                product.add(prod);
+                return new ResponseEntity<>(product, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        for(Product prod : products){
+            if(prod.getId() == id){
                 return new ResponseEntity<>(prod, HttpStatus.OK);
             }
         }
