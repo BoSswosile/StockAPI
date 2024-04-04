ad581b3438cc215eaf169133f35db59e0a89ba8b
clementg13 <grosieux.clement@gmail.com>
2024-04-04 10:45:38 +0200

    edit auth to disable cors for the web server

--- src/main/java/com/stockapi/StockAPI/controller/AuthController.java
+++ src/main/java/com/stockapi/StockAPI/controller/AuthController.java
@@ -2,19 +2,19 @@ package com.stockapi.StockAPI.controller;
 
 import com.stockapi.StockAPI.model.User;
 import com.stockapi.StockAPI.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     @Autowired
     UserService userService;
