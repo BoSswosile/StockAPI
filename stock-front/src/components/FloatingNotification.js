c08903229f28d92725e3dc232a38e6f4fd9a7b9e
clementg13 <grosieux.clement@gmail.com>
2024-04-13 01:04:46 +0200

    patch notification

--- stock-front/src/components/FloatingNotification.js
+++ stock-front/src/components/FloatingNotification.js
@@ -1,27 +1,29 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const FloatingNotification = () => {
     const [message, setMessage] = useState('');
     const [showNotification, setShowNotification] = useState(false);
 
     useEffect(() => {
+        if (typeof window == 'undefined') {
+            return  null;
+        }
         const storedMessage = localStorage.getItem('message');
         if (storedMessage) {
             setMessage(storedMessage);
             setShowNotification(true);
-
-            const timer = setTimeout(() => {
-                setShowNotification(false);
-                localStorage.removeItem('message'); // Supprime le message de localStorage
-            }, 5000); // 3000 millisecondes = 3 secondes
-
-            // Nettoyage de l'effet de bord pour éviter les fuites de mémoire
-            return () => clearTimeout(timer);
         }
     }, []);
+
+    const handleClose = () => {
+        if (typeof window == 'undefined') {
+            return  null;
+        }
+        setShowNotification(false);
+        localStorage.removeItem('message'); // Supprime le message de localStorage
+    };
 
     if (!showNotification) {
         return null;
@@ -30,9 +32,9 @@ const FloatingNotification = () => {
     return (
         <div className="fixed top-0 right-0 m-4 p-4 bg-blue-500 text-white rounded-md shadow-lg z-50">
             {message}
+            <button onClick={handleClose} className="ml-2">X</button>
         </div>
     );
-
 };
 
 export default FloatingNotification;
