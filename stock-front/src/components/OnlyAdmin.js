ac915d44c2e1bc637e734b9858418458f5800f42
clementg13 <grosieux.clement@gmail.com>
2024-04-13 01:04:18 +0200

    add admin restriction

---
+++ stock-front/src/components/OnlyAdmin.js
@@ -0,0 +1,29 @@
+'use client'
+
+import {useRouter} from "next/navigation";
+
+const OnlyAdmin = () => {
+    const router = useRouter();
+
+    if (typeof window == 'undefined') {
+        return  null;
+    }
+
+    // check if the user have the role in localstorage
+    const jwt = localStorage.getItem('jwt');
+
+    if (!jwt) {
+        localStorage.setItem('message', "Vous n'êtes pas connecté");
+        router.push('/auth/login');
+    }
+
+    if(localStorage.getItem('role') !== 'ROLE_ADMINISTRATOR') {
+        localStorage.setItem('message', "Vous n'êtes pas autorisé à accéder à cette page");
+        router.push('/');
+    }
+
+    return null;
+
+};
+
+export default OnlyAdmin;
