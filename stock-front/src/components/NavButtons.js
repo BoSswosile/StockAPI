e98cc3dc278770b8a68a18a7e46f566ea6cb1b02
clementg13 <grosieux.clement@gmail.com>
2024-04-18 11:57:50 +0200

    patch localstorage

--- stock-front/src/components/NavButtons.js
+++ stock-front/src/components/NavButtons.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 const NavButtons = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [showNav, setShowNav] = useState(false);
 
     useEffect(() => {
         // Vérifie si le token JWT est présent dans le localStorage
@@ -13,6 +14,7 @@ const NavButtons = () => {
         }
         const token = localStorage.getItem('jwt');
         setIsLoggedIn(!!token);
+        setShowNav(true);
     }, []);
 
     const handleLogout = () => {
@@ -23,14 +25,21 @@ const NavButtons = () => {
     };
 
     const showUserButton = () => {
-        if (localStorage && localStorage.getItem('role') && localStorage.getItem('role') == "ROLE_ADMINISTRATOR") {
-            return (
-                <Link href="/user" className="text-sm mr-5 font-medium text-blue-600 hover:text-blue-500">Utilisateurs</Link>
-            );
-        }else {
-            return null;
+        // Vérifie si le code s'exécute côté client
+        if (typeof window !== 'undefined') {
+            const role = localStorage.getItem('role');
+            if (role && role === "ROLE_ADMINISTRATOR") {
+                return (
+                    <Link href="/user" className="text-sm mr-5 font-medium text-blue-600 hover:text-blue-500">Utilisateurs</Link>
+                );
+            }
         }
+        return null;
     };
+
+    if (!showNav) {
+        return null;
+    }
 
     return (
         <div className="flex justify-center items-center space-x-4 my-20">
@@ -40,16 +49,19 @@ const NavButtons = () => {
             {showUserButton()}
             {!isLoggedIn && (
                 <>
-                    <Link href="/auth/login" className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <Link href="/auth/login"
+                          className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Connexion
                     </Link>
-                    <Link href="/auth/register" className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <Link href="/auth/register"
+                          className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Inscrire
                     </Link>
                 </>
             )}
             {isLoggedIn && (
-                <Link href="/auth/logout" className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Link href="/auth/logout"
+                      className="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Deconnexion
                 </Link>
             )}
